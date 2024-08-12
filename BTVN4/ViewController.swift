28c3bb5ccb4a6c6c0aaadb90c4c358f0a50ce26f
quynhtt <quynhtt@newwave.vn>
2024-08-12 20:39:54 +0700

    init project

---
+++ BTVN4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTVN4
+//
+//  Created by Tao on 8/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
